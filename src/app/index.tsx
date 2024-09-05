92bd6b0b1a105c896f462089e0deb4e82ce92b4e
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-09-05 21:00:32 +0700

    change staging

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <h1 className="text-5xl font-bold">
-      Hello from the app <span className="text-red-500">staging</span>!
+      Hello from the app <span className="text-red-500">Another staging</span>!
     </h1>
   );
 }
