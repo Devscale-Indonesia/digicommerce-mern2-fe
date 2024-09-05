3109480c35e6898ecc8b0d56a65808d9600988a1
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-09-05 20:24:40 +0700

    create staging

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-5xl font-bold">Hello from the app!</h1>;
+  return (
+    <h1 className="text-5xl font-bold">
+      Hello from the app <span className="text-red-500">staging</span>!
+    </h1>
+  );
 }
